ef691473392764d75d1b61cf6f3b6dd9286b44f8
kim-cheolwoon <kimcheolwoon@allips.kr>
2024-11-28 13:51:45 +0900

    [design] 로그인 페이지/  약관동의 페이지

--- src/views/login/JoinAgree.vue
+++ src/views/login/JoinAgree.vue
@@ -1,11 +1,76 @@
 <template>
+    <Dialog v-model:visible="visible"
+        pt:root:class="!border-0 !bg-transparent w-full md:w-[500px] md:h-auto h-full !max-h-screen" 
+        pt:mask:class="backdrop-blur-sm bg-white">
+            <template #container>
+                <BackHeader title="약관동의"/>
+                <div class="modal-page-padding">
+                    <h2 class="text-xl font-bold"> <span class="text-indigo-600">플랜오더</span> <br> 약관에 동의해주세요.</h2>
+                    <p class="mt-3 mb-6 text-sm font-bold text-gray-400">모든 이용자는 플랜오더의 이용약관 및 개인정보 처리 방침에 동의해야합니다.</p>
 
-</template>
+                    <ul class="text-gray-600 flex flex-col gap-2 *:flex *:justify-between">
+                        <li class="border-b !pb-3">
+                            <div class="label-checkbox-box">
+                                <Checkbox></Checkbox>
+                                <label for="">모두 동의합니다.</label>
+                            </div>
+                        </li>
 
-<script setup lang="ts">
+                        <li class="mt-2">
+                            <div class="label-checkbox-box">
+                                <Checkbox></Checkbox>
+                                <label for="">[필수] 만 14세 이상입니다.</label>
+                            </div>
+                            <Button secondary text label="보기 >" size="small" severity="secondary" class="flex-none"/>
+                        </li>
 
-</script>
+                        <li>
+                            <div class="label-checkbox-box">
+                                <Checkbox></Checkbox>
+                                <label for="">[필수] 이용약관 동의</label>
+                            </div>
+                            <Button secondary text label="보기 >" size="small" severity="secondary" class="flex-none"/>
+                        </li>
 
-<style lang="scss">
+                        <li >
+                            <div class="label-checkbox-box">
+                                <Checkbox></Checkbox>
+                                <label for="">[필수] 개인정보 수집 - 이용 동의</label>
+                            </div>
+                            <Button secondary text label="보기 >" size="small" severity="secondary" class="flex-none"/>
 
-</style>
+                        </li>
+
+                        <li class="">
+                            <div class="label-checkbox-box">
+                                <Checkbox></Checkbox>
+                                <label for="">[선택사항] 마케팅 정보 수신 동의</label>
+                            </div>
+                            <Button secondary text label="보기 >" size="small" severity="secondary" class="flex-none"/>
+                        </li>
+                    </ul>
+                    <div class="mobile-fiex-bottom">
+                        <Button label="동의하고 계속하기" class="w-full" />
+                    </div>
+                </div>
+            </template>
+        </Dialog>
+    </template>
+    
+    <script setup lang="ts">
+    import Dialog from 'primevue/dialog';
+    import Checkbox from 'primevue/checkbox';
+
+    import { ref } from 'vue';
+    
+    import BackHeader from '@/components/layouts/BackHeader.vue'
+    const visible   = ref(true);
+    const idData = ref('')
+    const pwData = ref('')
+    
+    
+    </script>
+    
+    <style lang="scss" scoped>
+    
+    </style>
--- src/views/login/JoinFirst.vue
+++ src/views/login/JoinFirst.vue
@@ -1,11 +1,62 @@
 <template>
+    <Dialog v-model:visible="visible"
+        pt:root:class="!border-0 !bg-transparent w-full md:w-[500px] md:h-auto h-full !max-h-screen" 
+        pt:mask:class="backdrop-blur-sm bg-white">
+            <template #container>
+                <BackHeader title="매장회원가입 1/2"/>
+                <div class="modal-page-padding">
+                    <div class="form-gap-box">
+                        <IftaLabel class="w-full">
+                            <label for="emali">이메일</label>
+                            <InputText id="emali" placeholder="이메일을 입력해주세요." class="w-full"/>    
+                        </IftaLabel>
 
-</template>
+                        <IftaLabel class="w-full">
+                            <label for="emali">비밀번호</label>
+                            <InputText id="emali" placeholder="비밀번호를 입력해주세요" class="w-full"/>    
+                        </IftaLabel>
 
+                        <IftaLabel class="w-full">
+                            <label for="emali">비밀번호 확인</label>
+                            <InputText id="emali" placeholder="비밀번호를 다시 한 번 입력해주세요" class="w-full"/>    
+                        </IftaLabel>
+
+                        <IftaLabel>
+                            <IconField>
+                                <InputText id="username" placeholder="사업자 등록증을 업로드 해주세요." variant="filled" class="w-full" readonly/>
+                                <InputIcon class="pi pi-file-arrow-up" />
+                            </IconField>
+                            <label for="username">사업자 등록증 업로드</label>
+                        </IftaLabel>
+                    </div>
+                    <div class="mobile-fiex-bottom">
+                        <Button label="동의하고 계속하기" class="w-full" />
+                    </div>
+                </div>
+            </template>
+        </Dialog>
+    </template>
+    
 <script setup lang="ts">
+import Dialog from 'primevue/dialog';
+import Checkbox from 'primevue/checkbox';
+
+import IftaLabel from 'primevue/iftalabel';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+
+
+
+import { ref } from 'vue';
+
+import BackHeader from '@/components/layouts/BackHeader.vue'
+const visible   = ref(true);
+const idData = ref('')
+const pwData = ref('')
+
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 </style>
