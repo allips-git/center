1b393b38c6fb89c8a9c7792fcb5c6b1a04c3333d
kim-cheolwoon <kimcheolwoon@allips.kr>
2024-12-09 09:23:55 +0900

    [design] 설정 / 통계 페이지 퍼블리싱

--- src/views/setting/StaticPayUpdate.vue
+++ src/views/setting/StaticPayUpdate.vue
@@ -1,8 +1,45 @@
 <template>
+    <BackHeader title="고정비용 등록"/>
+    <main class="p-5 pb-20">
+        <section class="form-gap-box">
+            <IftaLabel class="w-full">
+                <InputText id="username" class="w-full"/>
+                <label for="emali">고정비용 이름</label>
+            </IftaLabel>
+
+            <IftaLabel class="w-full">
+                <DatePicker showIcon fluid iconDisplay="input" dateFormat="yy-mm-dd"  showTime hourFormat="24"/>
+                <label for="emali">고정 지출 일자</label>
+            </IftaLabel>
+
+            <IftaLabel class="w-full">
+                <InputText id="username" class="w-full"/>
+                <label for="emali">금액</label>
+            </IftaLabel>
+
+            <IftaLabel class="w-full">
+                <Select class="w-full"/>
+                <label for="emali">분류선택</label>
+            </IftaLabel>
+
+            <IftaLabel class="w-full">
+                <label for="emali">메모</label>
+                <Textarea  autoResize rows="3" cols="30" class="w-full" />
+            </IftaLabel>
+        </section>
+    </main>
+    <div class="bottom-fixed-btn-box">
+        <Button label="저장" size="large"/>
+    </div>
 
 </template>
 
 <script setup lang="ts">
+import BackHeader from '@/components/layouts/BackHeader.vue'
+import IftaLabel from 'primevue/iftalabel';
+import DatePicker from 'primevue/datepicker';
+import Textarea from 'primevue/textarea';
+
 
 </script>
 
