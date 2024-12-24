540f84b8e615e7de2701f39d8e878199451103a9
Wonmyong <49638622+Wonmyong@users.noreply.github.com>
2024-12-24 17:32:52 +0900

    [front] 통계 화면 처리(계속)

--- src/views/average/AverageView.vue
+++ src/views/average/AverageView.vue
@@ -83,28 +83,16 @@
         <section class="my-4">
             <h2 class="px-4 pt-2 text-lg font-bold">결제 수단</h2>
             <ul class="flex flex-col gap-2 p-4 mx-4 mt-4 border border-gray-200 rounded-md">
-                <li class="flex justify-between gap-5">
+                <li v-for="(item, index) in aver['payList']" :key="index" class="flex justify-between gap-5">
                     <div class="flex items-center gap-2">
-                        <span class="block bg-green-400 rounded-full size-2"></span>
-                        <p>계좌 0회</p>
+                        <span v-if="item['codeSub'] === '001'" class="block bg-green-400 rounded-full size-2"></span>
+                        <span v-if="item['codeSub'] === '002'" class="block rounded-full bg-sky-400 size-2"></span>
+                        <span v-if="item['codeSub'] === '003'" class="block bg-red-400 rounded-full size-2"></span>
+                        <p>{{ item['codeNm'] }} {{ item['cnt'] }}회</p>
                     </div>
-                    <p class="text-lg font-bold text-green-400">12,000원</p>
-                </li>
-
-                <li class="flex justify-between gap-5">
-                    <div class="flex items-center gap-2">
-                        <span class="block rounded-full bg-sky-400 size-2"></span>
-                        <p>현금 0회</p>
-                    </div>
-                    <p class="text-lg font-bold text-sky-400">12,000원</p>
-                </li>
-
-                <li class="flex justify-between gap-5">
-                    <div class="flex items-center gap-2">
-                        <span class="block bg-red-400 rounded-full size-2"></span>
-                        <p>카드 0회</p>
-                    </div>
-                    <p class="text-lg font-bold text-red-400">12,000원</p>
+                    <p v-if="item['codeSub'] === '001'" class="text-lg font-bold text-green-400">{{ getAmt(item['amt']) }}원</p>
+                    <p v-if="item['codeSub'] === '002'" class="text-lg font-bold text-sky-400">{{ getAmt(item['amt']) }}원</p>
+                    <p v-if="item['codeSub'] === '003'" class="text-lg font-bold text-red-400">{{ getAmt(item['amt']) }}원</p>
                 </li>
             </ul>
         </section>
@@ -120,16 +108,10 @@
                 <li class="flex justify-between gap-5">
                     <div class="flex items-center gap-2">
                         <span class="block bg-green-400 rounded-full size-2"></span>
+                        <span class="block rounded-full bg-sky-400 size-2"></span>
                         <p>블라인드 (1개)</p>
                     </div>
                     <p class="text-lg font-bold text-green-400">12,000원</p>
-                </li>
-
-                <li class="flex justify-between gap-5">
-                    <div class="flex items-center gap-2">
-                        <span class="block rounded-full bg-sky-400 size-2"></span>
-                        <p>커튼 (2개)</p>
-                    </div>
                     <p class="text-lg font-bold text-sky-400">12,000원</p>
                 </li>
             </ul>
