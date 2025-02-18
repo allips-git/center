c74993aea09e298b2cc7b8a2cce41cf1b9eb597a
Wonmyong <49638622+Wonmyong@users.noreply.github.com>
2025-02-18 10:26:09 +0900

    [front] 시공시간 설정 화면 기능 추가

---
+++ src/store/modules/time.ts
@@ -0,0 +1,36 @@
+/**
+ * @description 시공시간 관련 모듈 pinia
+ */
+import { defineStore } from 'pinia';
+import { getAxiosData } from '@/assets/js/function';
+
+interface State {
+    curtainTime : number;
+    blindTime   : number;
+}
+
+export const useTimeStore = defineStore('time', {
+    state: (): State => ({
+        curtainTime : 0,
+        blindTime   : 0
+    }),
+    actions : {
+        async getTime()
+        {
+            try
+            {
+                const instance  = await getAxiosData();
+                const res       = await instance.post(`https://data.planorder.kr/timeSettingV1/getTime`);
+
+                console.log(res);
+
+                this.curtainTime    = res.data['curtainTime'];
+                this.blindTime      = res.data['blindTime'];
+            }
+            catch(e)
+            {
+                console.log(e);
+            }
+        }
+    }
+});
