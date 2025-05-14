e30f80b067940c7527d67be29d51935212286f8c
Wonmyong <49638622+Wonmyong@users.noreply.github.com>
2025-05-14 14:53:07 +0900

    사이드바 고정활성처리

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ import ConfirmDialog from 'primevue/confirmdialog';
 
 const kakao = useKakaoStore();
 // const popup = usePopupStore();
-const setWideSide   = ref(false); // wideSide 상태를 정의
+const setWideSide   = ref(true); // wideSide 상태를 정의
 const listRef       = ref<HTMLElement | null>(null);
 
 const toggleHeader = () => {
